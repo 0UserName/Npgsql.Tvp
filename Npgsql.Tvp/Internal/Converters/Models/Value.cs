d09f154a5e2d3a28310dca60dce1fc0fb9e322fc
0username <net.fedorax64@gmail.com>
2025-12-04 13:17:37 +0300

    temp

---
+++ Npgsql.Tvp/Internal/Converters/Models/Value.cs
@@ -0,0 +1,76 @@
+﻿using Npgsql.Internal;
+
+using System;
+
+namespace Npgsql.Tvp.Internal.Converters.Models
+{
+    internal readonly struct Value(object item, PgConverterResolution resolution, Size writeRequirement)
+    {
+        /// <summary>
+        /// Size of the item headers.
+        /// </summary
+        /// 
+        /// <remarks>
+        /// OID + Length.
+        /// </remarks>
+        public const int METADATA_SIZE = sizeof(uint) + sizeof(int);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public object Item
+        {
+            get => item;
+        }
+
+        /// <summary>
+        /// The data type's OID - a unique id identifying 
+        /// the data type in a given database (in pg_type).
+        /// </summary>
+        public uint OID
+        {
+            get => resolution.PgTypeId.Oid.Value;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public PgConverter Converter
+        {
+            get => resolution.Converter;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Size WriteRequirement
+        {
+            get => writeRequirement;
+        }
+
+        /// <summary>
+        /// Size of the item.
+        /// </summary
+        public int Size
+        {
+            get;
+            init => GetSize(item, resolution, writeRequirement);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public object WriteState
+        {
+            get;
+            init => field = GetSize(item, resolution, writeRequirement);
+        }
+
+        private int GetSize(object item, PgConverterResolution resolution, Size writeRequirement)
+        {
+            object writeState = default;
+
+            return item is DBNull or null ? Constants.NULL_SIZE : Accessors.GetSizeOrDbNullAsObject(default, resolution.Converter, DataFormat.Binary, writeRequirement, item, ref writeState).Value.Value;
+        }
+    }
+}
---
+++ Npgsql.Tvp/Internal/Converters/ParameterWriter.cs
@@ -0,0 +1,80 @@
+﻿using Npgsql.Internal;
+
+using Npgsql.Tvp.Internal.Converters.Models;
+using Npgsql.Tvp.Internal.Converters.Models.Contracts;
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Npgsql.Tvp.Internal.Converters
+{
+    internal static class ParameterWriter
+    {
+        public static async ValueTask WriteAsync(PgWriter writer, CancellationToken cancellationToken)
+        {
+            using (IParameter parameter = (IParameter)writer.Current.WriteState)
+            {
+                // Sum of the sizes of the
+                // nearest writable values.
+                if (writer.ShouldFlush(parameter.MetadataSize))
+                {
+                    await writer.FlushAsync(cancellationToken).ConfigureAwait(default);
+                }
+
+
+                writer.WriteInt32(Constants.DIMENSIONS);
+                writer.WriteInt32(Constants.FLAGS);
+                writer.WriteUInt32(parameter.OID);
+                writer.WriteInt32(parameter.RowsCount);
+                writer.WriteInt32(Constants.LOWER_BOUND);
+
+
+                for (int i = 0; i < parameter.RowsCount; i++)
+                {
+                    // Sum of the sizes of the
+                    // nearest writable values:
+                    //
+                    // 1. Row size
+                    // 2. Columns count
+                    if (writer.ShouldFlush(sizeof(int) + sizeof(int)))
+                    {
+                        await writer.FlushAsync(cancellationToken).ConfigureAwait(default);
+                    }
+
+
+                    writer.WriteInt32(parameter.GetRowSize(i));
+                    writer.WriteInt32(parameter.ColumnsCount);
+
+
+                    for (int j = 0; j < parameter.ColumnsCount; j++)
+                    {
+                        // Sum of the sizes of the
+                        // nearest writable values:
+                        //
+                        // 1. Item OID
+                        // 2. Item size
+                        if (writer.ShouldFlush(sizeof(uint) + sizeof(int)))
+                        {
+                            await writer.FlushAsync(cancellationToken).ConfigureAwait(default);
+                        }
+
+
+                        Value value = parameter[i, j];
+
+                        writer.WriteUInt32(value.OID);
+                        writer.WriteInt32(value.Size);
+
+
+                        if (value.Size != Constants.NULL_SIZE)
+                        {
+                            using (await writer.BeginNestedWriteAsync(value.WriteRequirement, value.Size, value.WriteState, cancellationToken).ConfigureAwait(default))
+                            {
+                                await Accessors.WriteAsObjectAsync(value.Converter, writer, value.Item, cancellationToken).ConfigureAwait(default);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
