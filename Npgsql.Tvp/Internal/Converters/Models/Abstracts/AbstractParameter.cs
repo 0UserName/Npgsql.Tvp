73246369312239c8f53dd5ec3873fabe1c937eeb
0username <net.fedorax64@gmail.com>
2025-12-09 03:04:15 +0300

    Initial commit

---
+++ Npgsql.Tvp/Internal/Converters/Models/Abstracts/AbstractParameter.cs
@@ -0,0 +1,94 @@
+﻿using Npgsql.Internal;
+using Npgsql.Internal.Postgres;
+
+using Npgsql.Tvp.Internal.Converters.Buffers;
+using Npgsql.Tvp.Internal.Converters.Models.Contracts;
+
+using System;
+
+namespace Npgsql.Tvp.Internal.Converters.Models.Abstracts
+{
+    internal abstract class AbstractParameter(string dataTypeName, int columnsCount, int rowsCount, PgSerializerOptions options) : IParameter
+    {
+        private readonly
+            Lazy<LinearValueBuffer> _buffer = new
+            Lazy<LinearValueBuffer>
+            (() => new LinearValueBuffer(columnsCount, rowsCount));
+
+        protected LinearValueBuffer Buffer
+        {
+            get => _buffer.Value;
+        }
+
+        /// <inheritdoc/>
+        public int ColumnsCount
+        {
+            get => columnsCount;
+        }
+
+        /// <inheritdoc/>
+        public int RowsCount
+        {
+            get => Buffer.Written / columnsCount;
+        }
+
+        /// <inheritdoc/>
+        public int MetadataSize
+        {
+            get => sizeof(int) + sizeof(int) + sizeof(uint) + Constants.DIMENSIONS * (sizeof(int) + sizeof(int)) + sizeof(int) * RowsCount;
+        }
+
+        /// <inheritdoc/>
+        public uint OID
+        {
+            get => options.GetArrayElementTypeId(new DataTypeName(dataTypeName).ToArrayName()).Oid.Value;
+        }
+
+        /// <inheritdoc/>
+        public Value this[int row, int column]
+        {
+            get => Buffer[row, column];
+        }
+
+        /// <summary>
+        /// Fills the buffer with parameter 
+        /// values for subsequent efficient 
+        /// and unified reuse.
+        /// </summary>
+        protected abstract void FillBuffer();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected Value CreateValue(object value, Type type)
+        {
+            PgTypeInfo pgTypeInfo = options.GetDefaultTypeInfo(type) ?? throw new InvalidOperationException($"{ type.FullName } is not supported");
+
+            PgConverterResolution resolution = pgTypeInfo.GetObjectResolution(value);
+
+            BufferRequirements bufferRequirements = pgTypeInfo.GetBufferRequirements(resolution.Converter, DataFormat.Binary) ?? throw new NotSupportedException($"Binary format is not supported: { resolution.PgTypeId }");
+
+            return new Value(value, resolution, bufferRequirements.Write);
+        }
+
+        /// <inheritdoc/>
+        public int GetSize()
+        {
+            FillBuffer();
+
+            return MetadataSize + Buffer.Size;
+        }
+
+        /// <inheritdoc/>
+        public int GetRowSize(int row)
+        {
+            return Buffer[row];
+        }
+
+        /// <inheritdoc/>
+        public virtual void Dispose()
+        {
+            Buffer.Dispose();
+        }
+    }
+}
---
+++ Npgsql.Tvp/Internal/Converters/Models/Value.cs
@@ -0,0 +1,78 @@
+﻿using Npgsql.Internal;
+
+using System;
+
+namespace Npgsql.Tvp.Internal.Converters.Models
+{
+    internal readonly struct Value(object value, PgConverterResolution resolution, Size writeRequirement)
+    {
+        private readonly (Size BufferRequirement, object WriteState) _state = GetSizeOrDbNullAsObject(value, resolution.Converter, writeRequirement);
+
+        /// <summary>
+        /// Size of the target value headers.
+        /// </summary
+        /// 
+        /// <remarks>
+        /// OID + Size.
+        /// </remarks>
+        public const int METADATA_SIZE = sizeof(uint) + sizeof(int);
+
+        /// <summary>
+        /// Target value.
+        /// </summary>
+        public object UnderlyingValue
+        {
+            get => value;
+        }
+
+        /// <summary>
+        /// Unique id identifying the data type in a given database (in pg_type).
+        /// </summary>
+        public uint OID
+        {
+            get;
+
+        } = resolution.PgTypeId.Oid.Value;
+
+        /// <summary>
+        /// Handles the logic for 
+        /// processing the target 
+        /// value.
+        /// </summary>
+        public PgConverter Converter
+        {
+            get;
+
+        } = resolution.Converter;
+
+        public Size BufferRequirement
+        {
+            get => _state.BufferRequirement;
+        }
+
+        /// <summary>
+        /// Not really used.
+        /// </summary>
+        public object WriteState
+        {
+            get => _state.WriteState;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// GetDefaultTypeInfo 
+        /// returns a non-nullable converter because DataTable does not support 
+        /// nullable column types. Explicit validation is required for accurate 
+        /// size calculation.
+        /// </remarks>
+        private static (Size, object) GetSizeOrDbNullAsObject(object item, PgConverter converter, Size writeRequirement)
+        {
+            object writeState = default;
+
+            return (item is DBNull or null ? Constants.NULL_SIZE : Accessors.GetSizeOrDbNullAsObject(default, converter, DataFormat.Binary, writeRequirement, item, ref writeState).Value, writeState);
+        }
+    }
+}
